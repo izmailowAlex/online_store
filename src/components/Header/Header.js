99549e7664e541f4f981dbf83fff4d7bd15deff8
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-22 00:37:07 +0400

    feat: change actions menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
-import Order from "./Order";
 import Logo from "../../images/logo.svg";
-import './Header.css';
+import Actions from "./Actions";
+import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__container container">
         <Nav />
-        <Link className="header__logo" to='/'><img src={Logo} alt="Balloon Logo"/></Link>
-        <Order />
+        <Link className="header__logo" to="/">
+          <img src={Logo} alt="Balloon Logo" />
+        </Link>
+        <Actions />
       </div>
     </header>
   );
