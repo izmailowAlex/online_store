28aef2ecb03f461479097610c9bf507960d6d7f7
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-21 23:57:27 +0400

    feat: add decorative elements for background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import './App.css';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Main />
         <Footer />
       </Router>
+      <span className="balloon__circle balloon_red"></span>
+      <span className="balloon__circle balloon_yellow"></span>
+      <span className="balloon__circle balloon_blue"></span>
     </div>
   );
 }
