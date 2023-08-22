6fbc6de3ff28746f228f91af4b1cc7467bc7b3c8
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-22 15:10:48 +0400

    feat: add ContextProvider & create & fetch data file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import data from "./data";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import './App.css';
+import "./App.css";
+
+export const AppContext = React.createContext();
 
 function App() {
+  const [productsLibrary, setProductsLibrary] = useState(data);
+
   return (
     <div className="balloon">
-      <Router>
-        <Header />
-        <Main />
-        <Footer />
-      </Router>
+      <AppContext.Provider value={{ productsLibrary, setProductsLibrary }}>
+        <Router>
+          <Header />
+          <Main />
+          <Footer />
+        </Router>
+      </AppContext.Provider>
       <span className="balloon__circle balloon_red"></span>
       <span className="balloon__circle balloon_yellow"></span>
       <span className="balloon__circle balloon_blue"></span>
