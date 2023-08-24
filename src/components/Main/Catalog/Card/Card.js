aa6c40c1c37d4f89238c529deea7660d4ea7ef04
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-24 12:41:14 +0400

    refactor: remove link in title to Card

--- src/components/Main/Catalog/Card/Card.js
+++ src/components/Main/Catalog/Card/Card.js
@@ -8,9 +8,7 @@ function Card({ product }) {
       <div className="card-image-wrapper">
         <img className="card-image__img" src={product.src} alt="" />
       </div>
-      <div className="card__title">
-        <Link to={`/productview/${product.id}/`}>{product.title}</Link>
-      </div>
+      <div className="card__title">{product.title}</div>
       <div className="card-controls">
         <h3 className="card-price">{product.price} Br</h3>
         <Counter min={1} max={product.count} />
