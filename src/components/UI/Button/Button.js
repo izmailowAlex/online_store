68e9626e25a16a239d53bf6d2f97396e9b839485
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-24 12:57:32 +0400

    feat: add onClick event to Button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,16 +1,18 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./Button.css";
 
-function Button({ children, className, to, button }) {
+function Button({ children, className, to, button, onClick }) {
   const classname = className ? "button " + className : "button";
 
   return (
     <>
       {button ? (
-        <button className={classname}>{children}</button>
+        <button className={classname} onClick={onClick}>
+          {children}
+        </button>
       ) : (
-        <Link className={classname} to={to}>
+        <Link className={classname} onClick={onClick} to={to}>
           {children}
         </Link>
       )}
