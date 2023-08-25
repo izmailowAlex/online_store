73e2ad9abadb400178c82a44e2f43e6467ddb4cf
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-26 01:45:49 +0400

    feat: migration Card component to tsx

---
+++ src/components/Main/Catalog/Card/Card.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Counter from '../../../UI/Counter/Counter'
+import { ICardProps } from '../../../../interfaces/interface'
+import './Card.css'
+
+function Card ({ product }: ICardProps): JSX.Element {
+  return (
+    <div className="card">
+      <div className="card__image-wrapper">
+        <img
+          className="card__image"
+          src={`../images/${product.image}`}
+          alt=""
+        />
+      </div>
+      <div className="card__controls">
+        <h3 className="card__price">{product.price} ₽</h3>
+        <Counter count={product.count} min={product.min} max={product.max} />
+        <button className="card__button"></button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -19,3 +19,26 @@ export interface ICounterProps {
   min: number
   max: number
 }
+
+export interface ICardProps {
+  product: IProduct
+}
+
+export interface IProduct {
+  id: string
+  title: string
+  image: string
+  count: number
+  min: number
+  max: number
+  price: number
+  instock: boolean
+  discount: number
+  category: string
+  type: string
+  color: string
+  size: string
+  producer: string
+  country: string
+  description: string
+}
