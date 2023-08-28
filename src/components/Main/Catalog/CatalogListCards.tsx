250773557750ce504e8394aa94115abd57abab07
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-28 14:42:39 +0400

    feat: migration CatalogListCards component to tsx

--- src/components/Main/Catalog/CatalogListCards.tsx
+++ src/components/Main/Catalog/CatalogListCards.tsx
@@ -3,7 +3,7 @@ import React, { useContext } from 'react'
 import { CatalogContext } from './Catalog'
 import Card from './Card/Card'
 
-function CatalogListCards () {
+function CatalogListCards (): JSX.Element {
   const { filteredList } = useContext(CatalogContext)
 
   return (
