d0c2b721ad7fec2f9e2e22d8f40ccc5d76c623ec
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-28 23:22:27 +0400

    reactor: migrate Main component to tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,12 +4,11 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Home/Home'
 import Catalog from './Catalog/Catalog'
 import ProductView from './ProductView/ProductView'
-import Checkout from './Checkout/Checkout'
 import Page404 from './Page404/Page404'
 import Basket from './Basket/Basket'
 import './Main.css'
 
-function Main () {
+function Main (): JSX.Element {
   return (
     <main className="main">
       <div className="main__container container">
@@ -17,7 +16,6 @@ function Main () {
           <Route path="/" element={<Home />} />
           <Route path="/catalog/" element={<Catalog />} />
           <Route path="/productview/:id" element={<ProductView />} />
-          <Route path="/checkout/" element={<Checkout />} />
           <Route path="/page404/" element={<Page404 />} />
           <Route path="/cart/" element={<Basket />} />
         </Routes>
