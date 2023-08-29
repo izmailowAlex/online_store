2b32f05dca0860fe59996edb3d08911a6b657a73
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-08-29 16:43:21 +0400

    feat: migration Checkout component to tsx

--- src/components/Main/Checkout/Checkout.tsx
+++ src/components/Main/Checkout/Checkout.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { ICheckoutProps } from '../../../interfaces/interface'
 import Input from '../../UI/Input/Input'
 import Button from '../../UI/Button/Button'
 import './Checkout.css'
 
 function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
+  const [errors, setErrors] = useState(true)
+  const [confirm, setConfirm] = useState(false)
   const [errorName, setErrorName] = useState(false)
   const [errorPhone, setErrorPhone] = useState(false)
   const [errorAddress, setErrorAddress] = useState(false)
@@ -13,16 +15,42 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   const [errorCardNumber, setErrorCardNumber] = useState(false)
   const [errorCardDate, setErrorCardDate] = useState(false)
   const [errorCardCVC, setErrorCardCVC] = useState(false)
+  const checkoutFieldsRef = useRef<HTMLDivElement>(null)
 
   function confirmOrderHandler (): void {
-    console.log('Confirm Order')
+    const inputName = document.querySelector('[name=name]') as HTMLInputElement
+    checkNameHandler(inputName.value)
+    const inputPhone = document.querySelector('[name=phone]') as HTMLInputElement
+    checkPhoneHandler(inputPhone.value)
+    const inputAddress = document.querySelector('[name=address]') as HTMLInputElement
+    checkAddressHandler(inputAddress.value)
+    const inputEmail = document.querySelector('[name=email]') as HTMLInputElement
+    checkEmailHandler(inputEmail.value)
+    const inputCardholderName = document.querySelector('[name=cardHolderName]') as HTMLInputElement
+    checkCardholderName(inputCardholderName.value)
+    const inputCardNumber = document.querySelector('[name=cardNumber]') as HTMLInputElement
+    checkCardNumber(inputCardNumber.value)
+    const inputCardDate = document.querySelector('[name=cardDate]') as HTMLInputElement
+    checkCardDate(inputCardDate.value)
+    const inputCardCVC = document.querySelector('[name=cardCVC]') as HTMLInputElement
+    checkCardCVC(inputCardCVC.value)
+
+    if (errors) {
+      setConfirm(false)
+      console.log(confirm)
+    } else {
+      setConfirm(true)
+      console.log(confirm)
+    }
   }
 
   function checkNameHandler (value: string): void {
     const reg = /^[а-яА-Яa-zA-Z]+ [а-яА-Яa-zA-Z]+$/
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorName(false)
     } else {
+      setErrors(true)
       setErrorName(true)
     }
   }
@@ -30,17 +58,21 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   function checkPhoneHandler (value: string): void {
     const reg = /^((8|\+374|\+994|\+995|\+375|\+7|\+380|\+38|\+996|\+998|\+993)[\- ]?)?\(?\d{3,5}\)?[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}(([\- ]?\d{1})?[\- ]?\d{1})?$/ //eslint-disable-line
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorPhone(false)
     } else {
+      setErrors(true)
       setErrorPhone(true)
     }
   }
 
   function checkAddressHandler (value: string): void {
-    const reg = /^[а-яА-Яa-zA-Z\d,./ ]+$/
+    const reg = /^([а-яА-Яa-zA-Z./ ]+), ([а-яА-Яa-zA-Z\d./ ]+), ([а-яА-Яa-zA-Z\d,./ ]+)$/
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorAddress(false)
     } else {
+      setErrors(true)
       setErrorAddress(true)
     }
   }
@@ -48,8 +80,10 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   function checkEmailHandler (value: string): void {
     const reg = /^[A-Z\d._%+-]+@[A-Z\d-]+.[A-Z]{2,4}$/i
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorEmail(false)
     } else {
+      setErrors(true)
       setErrorEmail(true)
     }
   }
@@ -57,8 +91,10 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   function checkCardholderName (value: string): void {
     const reg = /^[A-Z]+ [A-Z]+$/
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorCardholderName(false)
     } else {
+      setErrors(true)
       setErrorCardholderName(true)
     }
   }
@@ -66,8 +102,10 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   function checkCardNumber (value: string): void {
     const reg = /^\d{16}$/
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorCardNumber(false)
     } else {
+      setErrors(true)
       setErrorCardNumber(true)
     }
   }
@@ -75,8 +113,10 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   function checkCardDate (value: string): void {
     const reg = /^\d{2}\/\d{2}$/
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorCardDate(false)
     } else {
+      setErrors(true)
       setErrorCardDate(true)
     }
   }
@@ -84,8 +124,10 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   function checkCardCVC (value: string): void {
     const reg = /^\d{3}$/
     if (value.match(reg) !== null) {
+      setErrors(false)
       setErrorCardCVC(false)
     } else {
+      setErrors(true)
       setErrorCardCVC(true)
     }
   }
@@ -93,97 +135,110 @@ function Checkout ({ setPopupWindow }: ICheckoutProps): JSX.Element {
   return (
     <div className="checkout">
       <div className="checkout__form">
-        <div className="checkout__wrapper">
-          <h2 className="checkout__title">Оформление заказа</h2>
-          <Input
-            className="checkout__input"
-            name="name"
-            placeholder="Имя Фамилия"
-            maxlength={30}
-            error={errorName}
-            errorMessage="Неверный формат имени"
-            onBlur={(event) => { checkNameHandler(event.target.value) }}
-          />
-          <Input
-            className="checkout__input"
-            name="phone"
-            placeholder="Телефон"
-            maxlength={14}
-            error={errorPhone}
-            errorMessage="Неверный формат телефона"
-            onBlur={(event) => { checkPhoneHandler(event.target.value) }}
-          />
-          <Input
-            className="checkout__input"
-            name="address"
-            placeholder="Адрес доставки"
-            maxlength={100}
-            error={errorAddress}
-            errorMessage="Неверный формат адреса"
-            onBlur={(event) => { checkAddressHandler(event.target.value) }}
-          />
-          <Input
-            className="checkout__input"
-            name="email"
-            placeholder="E-mail"
-            maxlength={50}
-            error={errorEmail}
-            errorMessage="Неверный формат E-mail"
-            onBlur={(event) => { checkEmailHandler(event.target.value) }}
-          />
-          <div className="checkout__credit-card credit-card">
-            <Input
-              className="credit-card__input"
-              name="cardHolderName"
-              placeholder="CARDHOLDER NAME"
-              maxlength={30}
-              error={errorCardholderName}
-              errorMessage="Неверный формат держателя карты"
-              onBlur={(event) => { checkCardholderName(event.target.value) }}
-            />
-            <Input
-              className="credit-card__input"
-              name="cardNumber"
-              placeholder="1234 5678 9012 3456"
-              maxlength={16}
-              error={errorCardNumber}
-              errorMessage="Неверный формат номера карты"
-              onBlur={(event) => { checkCardNumber(event.target.value) }}
-            />
-            <div className="credit-card__expiration">
+        {!confirm
+          ? <div className="checkout__wrapper">
+            <h2 className="checkout__title">Оформление заказа</h2>
+            <div ref={checkoutFieldsRef} className="checkout__fields">
               <Input
-                className="credit-card__input"
-                name="cardDate"
-                placeholder="MM / YY"
-                maxlength={5}
-                error={errorCardDate}
-                errorMessage="Неверная дата"
-                onBlur={(event) => { checkCardDate(event.target.value) }}
+                className="checkout__input"
+                name="name"
+                placeholder="Имя Фамилия"
+                maxlength={30}
+                error={errorName}
+                errorMessage="Неверный формат Имени Фамилии"
+                onBlur={(event) => { checkNameHandler(event.target.value) }}
               />
               <Input
-                className="credit-card__input"
-                name="cardCVC"
-                placeholder="CVC"
-                maxlength={3}
-                error={errorCardCVC}
-                errorMessage="Неверный CVC"
-                onBlur={(event) => { checkCardCVC(event.target.value) }}
+                className="checkout__input"
+                name="phone"
+                placeholder="+375 (__) ___-__-__"
+                maxlength={14}
+                error={errorPhone}
+                errorMessage="Неверный формат телефона"
+                onBlur={(event) => { checkPhoneHandler(event.target.value) }}
               />
+              <Input
+                className="checkout__input"
+                name="address"
+                placeholder="г. Минск, ул. 50 лет Победы, д. 1"
+                maxlength={100}
+                error={errorAddress}
+                errorMessage="Неверный формат адреса"
+                onBlur={(event) => { checkAddressHandler(event.target.value) }}
+              />
+              <Input
+                className="checkout__input"
+                name="email"
+                placeholder="email@example.com"
+                maxlength={50}
+                error={errorEmail}
+                errorMessage="Неверный формат E-mail"
+                onBlur={(event) => { checkEmailHandler(event.target.value) }}
+              />
+              <div className="checkout__credit-card credit-card">
+                <Input
+                  className="credit-card__input"
+                  name="cardHolderName"
+                  placeholder="CARDHOLDER NAME"
+                  maxlength={30}
+                  error={errorCardholderName}
+                  errorMessage="Неверный формат держателя карты"
+                  onBlur={(event) => { checkCardholderName(event.target.value) }}
+                />
+                <Input
+                  className="credit-card__input"
+                  name="cardNumber"
+                  placeholder="1234 5678 9012 3456"
+                  maxlength={16}
+                  error={errorCardNumber}
+                  errorMessage="Неверный формат номера карты"
+                  onBlur={(event) => { checkCardNumber(event.target.value) }}
+                />
+                <div className="credit-card__expiration">
+                  <Input
+                    className="credit-card__input"
+                    name="cardDate"
+                    placeholder="MM / YY"
+                    maxlength={5}
+                    error={errorCardDate}
+                    errorMessage="Неверная дата"
+                    onBlur={(event) => { checkCardDate(event.target.value) }}
+                  />
+                  <Input
+                    className="credit-card__input"
+                    name="cardCVC"
+                    placeholder="CVC"
+                    maxlength={3}
+                    error={errorCardCVC}
+                    errorMessage="Неверный CVC"
+                    onBlur={(event) => { checkCardCVC(event.target.value) }}
+                  />
+                </div>
+              </div>
             </div>
+            <Button
+              className="checkout__button-confirm"
+              onClick={() => { confirmOrderHandler() }}
+            >
+              Подтвердить
+            </Button>
           </div>
-        </div>
+          : <div className="checkout__wrapper">
+            <h2 className="checkout__title">Заказ подтвержден</h2>
+            <Button
+              className="checkout__button-confirm"
+              onClick={() => { setPopupWindow(false) }}
+            >
+              Продолжить покупки
+            </Button>
+          </div>
+        }
         <button
           className="checkout__button-close"
           onClick={() => {
             setPopupWindow(false)
           }}
         ></button>
-        <Button
-          className="checkout__button-confirm"
-          onClick={() => { confirmOrderHandler() }}
-        >
-          Подтвердить
-        </Button>
       </div>
     </div>
   )
