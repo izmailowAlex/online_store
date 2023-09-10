2b1ba5c4cdaf14856ea5a94b0d04de3616ed5de5
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-09-10 20:55:51 +0400

    feat: add condition display nav

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -7,7 +7,7 @@ function Nav (): JSX.Element {
   const location = useLocation()
   return (
     <nav className="nav">
-    {location.pathname !== '/catalog/' && (
+    {location.pathname !== '/catalog/' && location.pathname !== '/' && (
       <ul className="nav__list">
         <li className="nav__item">
           <Link className="nav__link" to="/catalog/">Каталог</Link>
