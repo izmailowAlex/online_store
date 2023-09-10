d0b948b53b219cd05952462c8bc8ea8bd808ed01
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-09-10 18:17:23 +0400

    fix: correct pathname in Home

--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -18,7 +18,7 @@ function Home (): JSX.Element {
         <p className="home__description">
           Воздушные шары на любой праздник. Доставляйте радость с нами!
         </p>
-        <Button to="/catalog">Смотреть каталог</Button>
+        <Button to="/catalog/">Смотреть каталог</Button>
       </div>
     </div>
   )
