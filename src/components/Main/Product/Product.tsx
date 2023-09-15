f96d5ca9b01af1ee0e99d1f94c82fc0a16a39f87
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-09-16 01:32:21 +0400

    refactor: make link to product view in cart

--- src/components/Main/Product/Product.tsx
+++ src/components/Main/Product/Product.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { AppContext } from '../../../context/context'
 import { IProductProps, ICartOrders } from '../../../interfaces/interface'
 import Checkbox from '../../UI/Checkbox/Checkbox'
@@ -42,7 +43,11 @@ function Product ({ id, image, title, price, count, min, max, istatusOrder, onCl
         <Checkbox id={id} onChange={onChangeFlag} isOrder={isOrder} changeCartOrdersContain={changeCartOrdersContain} />
       </span>
       <img className="product__image" src={`../images/${image}`} alt="Product"></img>
-      <span className="product__name">{title}</span>
+      <span className="product__title">
+        <Link className="product__title-link" to={`/productview/${id}/`}>
+          {title}
+        </Link>
+      </span>
       <span className="product__price">{price}</span>
       <span className="product__count">
         <Counter id={id} count={count} min={min} balance={currentBalance} setBalance={setCurrentBalance} changeCartOrdersContain={changeCartOrdersContain} />
