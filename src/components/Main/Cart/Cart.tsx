c00c2379359b15f72c43addd68a8ba9915316587
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-09-25 22:04:34 +0400

    fix: delete extra tabs in Cart

--- src/components/Main/Cart/Cart.tsx
+++ src/components/Main/Cart/Cart.tsx
@@ -89,81 +89,82 @@ function Cart (): JSX.Element {
       <div className="cart-content">
         {cartOrders.length !== 0
           ? (
-            <>
-              <div className="cart-table">
-                <div className="cart-table-upper">
-                  <h2 className="cart__title">Корзина</h2>
-                  <div
-                    className="cart-clear"
-                    onClick={() => { clearAllOrders() }}
-                  >
-                    Очистить корзину
-                  </div>
-                </div>
-                <div className="cart-list-head">
-                  <span className="cart-select-all">
-                    <Checkbox onChange={handleCheckedAllCartProducts} isOrder={checkedAllProducts} />
-                    <span>Выбрать все</span>
-                  </span>
-                </div>
-                <ul className="cart-list">
-                  {currentProductPageOfCartProducts.map((order) => {
-                    return (
-                      <Product
-                        key={order.id}
-                        id={order.id}
-                        title={order.title}
-                        image={order.image}
-                        price={order.price}
-                        count={order.order}
-                        min={order.min}
-                        max={order.max}
-                        istatusOrder={order.isOrder}
-                        setCheckedAllProducts={setCheckedAllProducts}
-                        onClick={() => { handleDeleteProduct(order.id) }}
-                      />
-                    )
-                  })}
-                </ul>
-                <div className="pagination">
-                  <ul className="page-list">
-                    <li className="page-item page-item-nav" onClick={() => { pageNavigate('prev') }} >Назад</li>
-                    <Pagination
-                      currentPage={currentCartPage}
-                      productsPerPage={productsPerCartPage}
-                      totalProducts={cartOrders.length}
-                      paginate={paginate}
-                    />
-                    <li className="page-item page-item-nav" onClick={() => { pageNavigate('next') }} >Вперёд</li>
-                  </ul>
-                </div>
+          <>
+          <div className="cart-table">
+            <div className="cart-table-upper">
+              <h2 className="cart__title">Корзина</h2>
+              <div
+                className="cart-clear"
+                onClick={() => { clearAllOrders() }}
+              >
+                Очистить корзину
               </div>
-              <div className="cart-summary">
-                <h2 className="cart-summary__title">Итого:</h2>
-                <p className="cart-amount">{String(success) === 'false' ? price.toFixed(2) : (price - (price * 0.2)).toFixed(2)}</p>
-                <Input
-                  className="cart-promocode"
-                  name="promocode"
-                  placeholder="Введите промокод E020PB3P"
-                  success={success}
-                  maxlength={8}
-                  onChange={(event) => { comparePromoCode(event.target.value) }}
+            </div>
+            <div className="cart-list-head">
+              <span className="cart-select-all">
+                <Checkbox onChange={handleCheckedAllCartProducts} isOrder={checkedAllProducts} />
+                <span>Выбрать все</span>
+              </span>
+            </div>
+            <ul className="cart-list">
+              {currentProductPageOfCartProducts.map((order) => {
+                return (
+                  <Product
+                    key={order.id}
+                    id={order.id}
+                    title={order.title}
+                    image={order.image}
+                    price={order.price}
+                    count={order.order}
+                    min={order.min}
+                    max={order.max}
+                    istatusOrder={order.isOrder}
+                    setCheckedAllProducts={setCheckedAllProducts}
+                    onClick={() => { handleDeleteProduct(order.id) }}
+                  />
+                )
+              })}
+            </ul>
+            <div className="pagination">
+              <ul className="page-list">
+                <li className="page-item page-item-nav" onClick={() => { pageNavigate('prev') }} >Назад</li>
+                <Pagination
+                  currentPage={currentCartPage}
+                  productsPerPage={productsPerCartPage}
+                  totalProducts={cartOrders.length}
+                  paginate={paginate}
                 />
-                <Button
-                  className="cart-checkout"
-                  onClick={() => { setPopupWindow(true) }}
-                >
-                  Оформить заказ
-                </Button>
-              </div>
-            </>)
-          : <>
-              <div className="cart-empty-block">
-                <h2 className="cart__title">Корзина пуста</h2>
-                <p className="cart-description">Выберите товар в каталоге</p>
-              </div>
-            </>
-          }
+                <li className="page-item page-item-nav" onClick={() => { pageNavigate('next') }} >Вперёд</li>
+              </ul>
+            </div>
+          </div>
+          <div className="cart-summary">
+            <h2 className="cart-summary__title">Итого:</h2>
+            <p className="cart-amount">{String(success) === 'false' ? price.toFixed(2) : (price - (price * 0.2)).toFixed(2)}</p>
+            <Input
+              className="cart-promocode"
+              name="promocode"
+              placeholder="Введите промокод E020PB3P"
+              success={success}
+              maxlength={8}
+              onChange={(event) => { comparePromoCode(event.target.value) }}
+            />
+            <Button
+              className="cart-checkout"
+              onClick={() => { setPopupWindow(true) }}
+            >
+              Оформить заказ
+            </Button>
+          </div>
+          </>)
+          : (
+          <>
+          <div className="cart-empty-block">
+            <h2 className="cart__title">Корзина пуста</h2>
+            <p className="cart-description">Выберите товар в каталоге</p>
+          </div>
+          </>)
+        }
       </div>
       {String(popupWindow) !== 'false' ? <Checkout setPopupWindow={setPopupWindow} /> : ''}
     </div>
